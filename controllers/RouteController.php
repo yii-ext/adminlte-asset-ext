f66cb85ee06bc39810c9e232643af483dbe6fca8
tangbq <tangbq@tiens.com>
2019-05-22 14:26:48 +0800

    iiiiiiiii

---
+++ controllers/RouteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace yiiapps\adminlte\controllers;
+
+use mdm\admin\models\User;
+use yii\web\Controller;
+
+/**
+ * User controller
+ */
+class RouteController extends \mdm\admin\controllers\RouteController
+{
+}
--- views/layouts/left.php
+++ views/layouts/left.php
@@ -1,3 +1,20 @@
+<?php
+use mdm\admin\components\MenuHelper;
+$menus = [
+    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
+    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
+    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
+    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+];
+$menus2 = MenuHelper::getAssignedMenu(Yii::$app->user->id);
+foreach ($menus2 as $key => $value) {
+    $value['icon'] = 'share';
+    $value['url'] = '#';
+    $menus2[$key] = $value;
+}
+$menus = array_merge($menus, $menus2);
+// var_dump($menus);exit;
+?>
 <aside class="main-sidebar">
 
     <section class="sidebar">
@@ -5,7 +22,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+                <img src="<?=$directoryAsset;?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
                 <p>Alexander Pierce</p>
@@ -26,43 +43,12 @@
         </form>
         <!-- /.search form -->
 
-        <?= dmstr\widgets\Menu::widget(
-            [
-                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
-                'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Some tools',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ]
-        ) ?>
+        <?=dmstr\widgets\Menu::widget(
+    [
+        'options' => ['class' => 'sidebar-menu tree', 'data-widget' => 'tree'],
+        'items' => $menus,
+    ]
+);?>
 
     </section>
 
