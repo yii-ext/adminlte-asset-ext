4b419bb9076f7a8d1256b581553cefda58dffcb7
tangbq <tangbq@tiens.com>
2019-08-28 11:36:34 +0800

    iii

--- views/menu/index.php
+++ views/menu/index.php
@@ -1,7 +1,7 @@
 <?php
 
-use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\helpers\Html;
 use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
@@ -13,34 +13,35 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="menu-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]);  ?>
+    <h1><?=Html::encode($this->title);?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]);  ;?>
 
     <p>
-        <?= Html::a(Yii::t('rbac-admin', 'Create Menu'), ['create'], ['class' => 'btn btn-success']) ?>
+        <?=Html::a(Yii::t('rbac-admin', 'Create Menu'), ['create'], ['class' => 'btn btn-success']);?>
     </p>
 
-    <?php Pjax::begin(); ?>
+    <?php Pjax::begin();?>
     <?=
-    GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'name',
-            [
-                'attribute' => 'menuParent.name',
-                'filter' => Html::activeTextInput($searchModel, 'parent_name', [
-                    'class' => 'form-control', 'id' => null
-                ]),
-                'label' => Yii::t('rbac-admin', 'Parent'),
-            ],
-            'route',
-            'order',
-            ['class' => 'yii\grid\ActionColumn'],
+GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
+        'name',
+        [
+            'attribute' => 'menuParent.name',
+            'filter' => Html::activeTextInput($searchModel, 'parent_name', [
+                'class' => 'form-control', 'id' => null,
+            ]),
+            'label' => Yii::t('rbac-admin', 'Parent'),
         ],
-    ]);
-    ?>
-<?php Pjax::end(); ?>
+        'route',
+        'order',
+        'visible',
+        ['class' => 'yii\grid\ActionColumn'],
+    ],
+]);
+?>
+<?php Pjax::end();?>
 
 </div>
