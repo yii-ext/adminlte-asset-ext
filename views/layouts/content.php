a19487c66acd2368bdb3168cfaf716b1ecc872b4
tangbq <tangbq@tiens.com>
2019-09-24 15:05:24 +0800

    ii

--- views/layouts/content.php
+++ views/layouts/content.php
@@ -8,15 +8,15 @@ use dmstr\widgets\Alert;
         <?=$content;?>
     </section>
 </div>
-
+<?php /*
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 2.0
-    </div>
-    <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-    reserved.
+<div class="pull-right hidden-xs">
+<b>Version</b> 2.0
+</div>
+<strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+reserved.
 </footer>
-
+ */;?>
 <!-- Control Sidebar -->
 <aside class="control-sidebar control-sidebar-dark">
     <!-- Create the tabs -->
--- views/layouts/left.php
+++ views/layouts/left.php
@@ -1,10 +1,11 @@
 <?php
 use yiiapps\adminlte\components\MenuHelper;
 $menus = [
-    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+    // ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
+    // ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
+    ['label' => '修改密码', 'icon' => 'file-code-o', 'url' => ['site/changePassword']],
+    // ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
+    // ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
 ];
 $menus2 = MenuHelper::getAssignedMenu(Yii::$app->user->id);
 foreach ($menus2 as $key => $value) {
@@ -17,29 +18,6 @@ $menus = array_merge($menus, $menus2);
 ?>
 <aside class="main-sidebar">
     <section class="sidebar">
-        <!-- Sidebar user panel -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="<?=$directoryAsset;?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
-            </div>
-            <div class="pull-left info">
-                <p>Alexander Pierce</p>
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
-
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
-        <!-- /.search form -->
-
         <?=yiiapps\adminlte\widgets\Menu::widget(
     [
         'options' => ['class' => 'sidebar-menu tree', 'data-widget' => 'tree'],
