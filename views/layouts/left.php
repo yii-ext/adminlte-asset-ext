10215e1a37fd4fdbf2c603a522f9affdfb7e2376
tbq <tbq2616@sina.com>
2019-10-10 16:02:21 +0800

    my zhanshen

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -3,14 +3,14 @@ use yiiapps\adminlte\components\MenuHelper;
 $menus = [
     // ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
     // ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-    ['label' => '修改密码', 'icon' => 'dashboard', 'url' => ['admin/user/change-password']],
+    ['label' => '修改密码', 'icon' => 'dashboard', 'url' => ['user/change-password']],
     // ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
     // ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
 ];
 $menus2 = MenuHelper::getAssignedMenu(Yii::$app->user->id);
 foreach ($menus2 as $key => $value) {
     $value['icon'] = 'share';
-    $value['url'] = '#';
+    $value['url'] = empty($value['items']) ? $value['url'] : '#';
     $menus2[$key] = $value;
 }
 $menus = array_merge($menus, $menus2);
